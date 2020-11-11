5a5f34681b5ba1df6362daf62b2ac2b114dd286a
jezzipan <jessica.leute@gmail.com>
2020-11-11 13:44:46 -0300

    remove

--- index.js
+++
@@ -1,9 +0,0 @@
-var input1 = document.getElementById("Num1");
-var input2 = document.getElementById("Num2");
-var button = document.getElementById("button");
-function sum(a, b) {
-    return a + b;
-}
-button.addEventListener("click", function () {
-    console.log(sum(Number(input1.value), Number(input2.value)));
-});
